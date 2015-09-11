96496cf865e96f743e304a18b1bef20203035441
Iskandar Ali <miskandarmali@gmail.com>
2015-09-11 18:08:40 +0800

    Add TempahanRequest

---
+++ app/Http/Requests/TempahanRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class TempahanRequest extends Request
+{
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		$rules = [
+			'nama' => 'required',
+		];
+
+		foreach($this->request->get('nama_penumpang') as $key => $val)
+		{
+
+			$rules['nama_penumpang.'.$key] = 'required';
+		}
+
+		return $rules;
+	}
+
+	public function messages()
+	{
+		$messages = [];
+		foreach($this->request->get('nama_penumpang') as $key => $val)
+		{
+		$messages['nama_penumpang.'.$key.'.max'] = 'The field labeled "Book Title '.$key.'" must be less than :max characters.';
+		}
+		return $messages;
+	}
+}
